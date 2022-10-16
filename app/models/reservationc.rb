a83cfac56eed1651892da7afb3de41cb46fb12b0
Sumio K <tap.miwo@gmail.com>
2022-10-16 11:39:56 +0900

    comp reserve sys

--- app/models/reservationc.rb
+++ app/models/reservationc.rb
@@ -1,2 +1,34 @@
 class Reservationc < ApplicationRecord
+  validates :name, presence: true
+  validate :date_before_start
+  validate :date_is_today
+  validate :start_time_not_sunday_saturday
+  validate :time_only
+  validates :start_time, uniqueness: {message: 'はほかのユーザーが予約しています'}
+
+
+
+  def date_before_start
+    if start_time < Date.today
+      errors.add(:start_time, "は過去の日付を選択できません")
+    end
+  end
+
+  def date_is_today
+    if start_time == Date.today
+      errors.add(:start_time, "は当日の予約は電話にてお願いします")
+    end
+  end
+
+  def start_time_not_sunday_saturday
+    if start_time.sunday? || start_time.saturday?
+      errors.add(:start_time, "土・日は定休日とさせていただいております")
+    end
+  end
+
+  def time_only
+    start_time.min == 00
+  end
+
+
 end
